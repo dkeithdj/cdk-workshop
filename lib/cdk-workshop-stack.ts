5fbf0a262e3a09712f550ecc1c026c8a2babd455
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-08-23 16:55:58 +0800

    feat: add lambda sample

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
-export class CdkWorkshopStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // defines an AWS Lambda resource
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_18_X, // execution environment
+      code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    const gateway = new LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
   }
 }
